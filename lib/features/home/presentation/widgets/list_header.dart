8cbc57910941a3271d50dc52b5de1870adb371c4
Leonard Pries <ge25quk@mytum.de>
2022-11-18 11:19:50 +0100

    update gitignore

---
+++ lib/features/home/presentation/widgets/list_header.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class ListHeader extends StatelessWidget {
+  const ListHeader({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(20.0),
+      height: 40,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            child: FittedBox(
+              child: Text(
+                "How can I help you?",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).colorScheme.tertiary,
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(width: 20),
+          const InfoButton()
+        ],
+      ),
+    );
+  }
+}
+
+class InfoButton extends StatelessWidget {
+  const InfoButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 42,
+      height: 22,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(50),
+        border: Border.all(
+          width: 3,
+          color: Theme.of(context).colorScheme.tertiary,
+        ),
+      ),
+      child: FittedBox(
+        child: Text(
+          "?",
+          style: TextStyle(
+            fontWeight: FontWeight.w800,
+            color: Theme.of(context).colorScheme.tertiary,
+          ),
+        ),
+      ),
+    );
+  }
+}
